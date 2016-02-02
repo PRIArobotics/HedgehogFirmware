68badcb8f37ea33a7821dc9794ab9f60e96e013e
Franky333 <1.daniel.frank@gmail.com>
2016-02-02 20:03:03 +0100

    added motor test to main

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include "digitalInput.h"
 #include "servo.h"
 #include "adc.h"
+#include "motor.h"
 
 
 int main()
@@ -13,13 +14,12 @@ int main()
 	digitalInput_init();
 	servo_init();
 	adc_init();
+	motor_init();
 
 
 	uint32_t i;
 
-
-	//uint8_t n;
-	//for(n=0; n<8; n++) adc_setPullup(n,true);
+	motor_setMode(0,MOTOR_MODE_FORWARD);
 
 	while(1)
 	{
@@ -41,10 +41,11 @@ int main()
 		for (i = 0; i < 10000000; i++);*/
 		if(!digitalInput_getState(0)) buzzer(true);
 		else buzzer(false);
-		servo_set(0,true,adc_getAnalogInput(0));
-		servo_set(1,true,adc_getAnalogInput(1));
-		servo_set(2,true,adc_getAnalogInput(2));
-		servo_set(3,true,adc_getAnalogInput(3));
+		motor_setPower(0,adc_getAnalogInput(0));
+		servo_set(0,true,adc_getAnalogInput(0)*2);
+		//servo_set(1,true,adc_getAnalogInput(1));
+		//servo_set(2,true,adc_getAnalogInput(2));
+		//servo_set(3,true,adc_getAnalogInput(3));
 		//speaker(adc_getAnalogInput(4)*20);
 		for (i = 0; i < 100000; i++);
 	}
