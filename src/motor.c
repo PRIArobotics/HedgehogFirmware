6f952c08e48fccc8e211c6a21e68723ecc3f0de9
Daniel Frank <1.daniel.frank@gmail.com>
2017-11-07 17:28:43 +0100

    changed motor to timer 2 to avoit conflict with servo

--- src/motor.c
+++ src/motor.c
@@ -43,28 +43,28 @@ void motor_init()
 	gpio_pinSet(pin_mode_01, false); //motor driver mode 0
 	gpio_pinSet(pin_mode_23, false); //motor driver mode 0
 
-	//timer 3 init
-	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; //enable clock (84MHz)
-	TIM3->PSC = 84; //prescaler = 84 --> 1MHz
-	TIM3->ARR = MOTOR_MAX_POWER - 1; //auto-reload value = 999 --> 1kHz PWM frequency
-	TIM3->CCMR1 |= (TIM_CCMR1_OC1PE | TIM_CCMR1_OC2PE); //channel 1+2 preload enable
-	TIM3->CCMR2 |= (TIM_CCMR2_OC3PE | TIM_CCMR2_OC4PE); //channel 3+4 preload enable
-	TIM3->DIER |= (TIM_DIER_UIE | TIM_DIER_CC1IE | TIM_DIER_CC2IE | TIM_DIER_CC3IE | TIM_DIER_CC4IE); //overflow & compare interrupts enable
-	TIM3->CCR1 = dutyCycle[0]; //motor 0 dutyCycle 0-MOTOR_MAX_POWER
-	TIM3->CCR2 = dutyCycle[1]; //motor 1 dutyCycle 0-MOTOR_MAX_POWER
-	TIM3->CCR3 = dutyCycle[2]; //motor 2 dutyCycle 0-MOTOR_MAX_POWER
-	TIM3->CCR4 = dutyCycle[3]; //motor 3 dutyCycle 0-MOTOR_MAX_POWER
-	TIM3->CR1 |= TIM_CR1_CEN; //enable timer
-	NVIC_EnableIRQ(TIM3_IRQn); //enable TIM3 global Interrupt
-	NVIC_SetPriority(TIM3_IRQn, 0); //highest interrupt priority
+	//timer 2 init
+	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN; //enable clock (84MHz)
+	TIM2->PSC = 84; //prescaler = 84 --> 1MHz
+	TIM2->ARR = MOTOR_MAX_POWER - 1; //auto-reload value = 999 --> 1kHz PWM frequency
+	TIM2->CCMR1 |= (TIM_CCMR1_OC1PE | TIM_CCMR1_OC2PE); //channel 1+2 preload enable
+	TIM2->CCMR2 |= (TIM_CCMR2_OC3PE | TIM_CCMR2_OC4PE); //channel 3+4 preload enable
+	TIM2->DIER |= (TIM_DIER_UIE | TIM_DIER_CC1IE | TIM_DIER_CC2IE | TIM_DIER_CC3IE | TIM_DIER_CC4IE); //overflow & compare interrupts enable
+	TIM2->CCR1 = dutyCycle[0]; //motor 0 dutyCycle 0-MOTOR_MAX_POWER
+	TIM2->CCR2 = dutyCycle[1]; //motor 1 dutyCycle 0-MOTOR_MAX_POWER
+	TIM2->CCR3 = dutyCycle[2]; //motor 2 dutyCycle 0-MOTOR_MAX_POWER
+	TIM2->CCR4 = dutyCycle[3]; //motor 3 dutyCycle 0-MOTOR_MAX_POWER
+	TIM2->CR1 |= TIM_CR1_CEN; //enable timer
+	NVIC_EnableIRQ(TIM2_IRQn); //enable TIM2 global Interrupt
+	NVIC_SetPriority(TIM2_IRQn, 0); //highest interrupt priority
 }
 
 
-void TIM3_IRQHandler(void)
+void TIM2_IRQHandler(void)
 {
-	if(TIM3->SR & TIM_SR_UIF) //counter overflow, start of pwm periods
+	if(TIM2->SR & TIM_SR_UIF) //counter overflow, start of pwm periods
 	{
-		TIM3->SR &= ~TIM_SR_UIF; //reset interrupt flag
+		TIM2->SR &= ~TIM_SR_UIF; //reset interrupt flag
 
 		uint8_t motor;
 		for(motor = 0; motor < 4; motor++) //for each motor
@@ -82,36 +82,36 @@ void TIM3_IRQHandler(void)
 		}
 	}
 
-	if(TIM3->SR & TIM_SR_CC1IF) //compare 1 interrupt flag, end of motor 0 pwm period
+	if(TIM2->SR & TIM_SR_CC1IF) //compare 1 interrupt flag, end of motor 0 pwm period
 	{
-		TIM3->SR &= ~TIM_SR_CC1IF;
+		TIM2->SR &= ~TIM_SR_CC1IF;
 		if(dutyCycle[0] < MOTOR_MAX_POWER)
 		{
 			gpio_pinSet(pin_a[0],false);
 			gpio_pinSet(pin_b[0],false);
 		}
 	}
-	if(TIM3->SR & TIM_SR_CC2IF) //compare 2 interrupt flag, end of motor 1 pwm period
+	if(TIM2->SR & TIM_SR_CC2IF) //compare 2 interrupt flag, end of motor 1 pwm period
 	{
-		TIM3->SR &= ~TIM_SR_CC2IF;
+		TIM2->SR &= ~TIM_SR_CC2IF;
 		if(dutyCycle[1] < MOTOR_MAX_POWER)
 		{
 			gpio_pinSet(pin_a[1],false);
 			gpio_pinSet(pin_b[1],false);
 		}
 	}
-	if(TIM3->SR & TIM_SR_CC3IF) //compare 3 interrupt flag, end of motor 2 pwm period
+	if(TIM2->SR & TIM_SR_CC3IF) //compare 3 interrupt flag, end of motor 2 pwm period
 	{
-		TIM3->SR &= ~TIM_SR_CC3IF;
+		TIM2->SR &= ~TIM_SR_CC3IF;
 		if(dutyCycle[2] < MOTOR_MAX_POWER)
 		{
 			gpio_pinSet(pin_a[2],false);
 			gpio_pinSet(pin_b[2],false);
 		}
 	}
-	if(TIM3->SR & TIM_SR_CC4IF) //compare 4 interrupt flag, end of motor 3 pwm period
+	if(TIM2->SR & TIM_SR_CC4IF) //compare 4 interrupt flag, end of motor 3 pwm period
 	{
-		TIM3->SR &= ~TIM_SR_CC4IF;
+		TIM2->SR &= ~TIM_SR_CC4IF;
 		if(dutyCycle[3] < MOTOR_MAX_POWER)
 		{
 			gpio_pinSet(pin_a[3],false);
@@ -133,8 +133,8 @@ void motor_setPower(uint8_t motor, uint16_t power)
 	if(motor >= MOTOR_COUNT) return;
 	if(power > MOTOR_MAX_POWER) power = MOTOR_MAX_POWER;
 	dutyCycle[motor] = power;
-	TIM3->CCR1 = dutyCycle[0];
-	TIM3->CCR2 = dutyCycle[1];
-	TIM3->CCR3 = dutyCycle[2];
-	TIM3->CCR4 = dutyCycle[3];
+	TIM2->CCR1 = dutyCycle[0];
+	TIM2->CCR2 = dutyCycle[1];
+	TIM2->CCR3 = dutyCycle[2];
+	TIM2->CCR4 = dutyCycle[3];
 }
