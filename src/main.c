f26717b048fa6f94536747fba2dedecc1a40d00e
Daniel Frank <1.daniel.frank@gmail.com>
2017-12-19 22:50:04 +0100

    changed main loop, startup nd shutdown

--- src/main.c
+++ src/main.c
@@ -9,7 +9,14 @@
 #include "motor.h"
 #include "uart.h"
 #include "hcp.h"
+#include "imu.h"
 //#include <core_cm4.h>
+
+
+#define SHUTDOWN_TIMEOUT 20
+
+
+bool rpi_was_active = false;
 
 
 int main()
@@ -17,44 +24,47 @@ int main()
 	//NVIC_SetPriorityGrouping(0); //TODO
 	systick_init();
 	gpio_init();
+	adc_init();
 	power_init();
 	output_init();
 
-	systick_busyWait(systick_timeToTicks(0, 0, 1, 0)); //wait 1s
+	systick_busyWait(systick_timeToTicks(0, 0, 1, 0));
 
 	power_on();
-	powerLed(true); //TODO: blinking until RPi booted?
+	powerLed(POWER_LED_MODE_BOOT);
 
-	speaker(500);
-	systick_busyWait(systick_timeToTicks(0, 0, 0, 200)); //wait 0.2s
-	speaker(2000);
-	systick_busyWait(systick_timeToTicks(0, 0, 0, 100)); //wait 0.1s
-	speaker(0);
+	speaker_startup();
 
 	power_regRpiEnable(true);
 	power_regMsEnable(true);
 
+	imu_init();
 	digitalIO_init();
 	servo_init();
-	adc_init();
 	motor_init();
 	uart_init();
 	hcp_init();
 
 	while(!power_getShutdown())
 	{
+		if(power_getRPiActive())
+		{
+			rpi_was_active = true;
+			powerLed(POWER_LED_MODE_ON);
+		}
+		if(rpi_was_active && (!power_getRPiActive())) break;
+
 		hcp_update();
 	}
 
-	speaker(2000);
-	systick_busyWait(systick_timeToTicks(0, 0, 0, 200)); //wait 0.2s
-	speaker(500);
-	systick_busyWait(systick_timeToTicks(0, 0, 0, 100)); //wait 0.1s
-	speaker(0);
-
-	powerLed(false); //TODO: blinking while RPi still active?
+	hcp_sendShutdown();
+	powerLed(POWER_LED_MODE_SHUTDOWN);
+	speaker_shutdown();
 	power_regMsEnable(false);
-	//TODO: wait for RPi shutdown?
+
+	uint64_t timeout = systick_getUptime() + systick_timeToTicks(0, 0, SHUTDOWN_TIMEOUT, 0);
+	while((power_getRPiActive()) && (systick_getUptime() < timeout));
+
 	power_regRpiEnable(false);
 	power_off();
 
