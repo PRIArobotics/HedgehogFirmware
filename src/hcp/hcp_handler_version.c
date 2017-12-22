80ca071855790ae7bebdf2bb06b160ee1d4db169
Franky333 <1.daniel.frank@gmail.com>
2017-12-22 13:56:59 +0100

    added hcp version request

--- src/hcp/hcp_handler_version.c
+++ src/hcp/hcp_handler_version.c
@@ -2,7 +2,26 @@
 #include "hcp_opcodes.h"
 #include "ringbuffer.h"
 
+#define STM32_UUID ((uint32_t *)0x1FFF7A10)
+
+#define HW_VERSION 2
+#define SW_VERSION 0
+
 void hcp_handler_version(hcp_conn_t conn, uint8_t opcode, size_t payloadLength)
 {
-	//TODO
+	ringbuffer_push(conn.txBuffer, HCP_VERS_REP);
+	ringbuffer_push(conn.txBuffer, (uint8_t)(STM32_UUID[2] >> 24));
+	ringbuffer_push(conn.txBuffer, (uint8_t)(STM32_UUID[2] >> 16));
+	ringbuffer_push(conn.txBuffer, (uint8_t)(STM32_UUID[2] >> 8));
+	ringbuffer_push(conn.txBuffer, (uint8_t)(STM32_UUID[2] >> 0));
+	ringbuffer_push(conn.txBuffer, (uint8_t)(STM32_UUID[1] >> 24));
+	ringbuffer_push(conn.txBuffer, (uint8_t)(STM32_UUID[1] >> 16));
+	ringbuffer_push(conn.txBuffer, (uint8_t)(STM32_UUID[1] >> 8));
+	ringbuffer_push(conn.txBuffer, (uint8_t)(STM32_UUID[1] >> 0));
+	ringbuffer_push(conn.txBuffer, (uint8_t)(STM32_UUID[0] >> 24));
+	ringbuffer_push(conn.txBuffer, (uint8_t)(STM32_UUID[0] >> 16));
+	ringbuffer_push(conn.txBuffer, (uint8_t)(STM32_UUID[0] >> 8));
+	ringbuffer_push(conn.txBuffer, (uint8_t)(STM32_UUID[0] >> 0));
+	ringbuffer_push(conn.txBuffer, HW_VERSION);
+	ringbuffer_push(conn.txBuffer, SW_VERSION);
 }
