feaba90fb37d8b49bc15f4a3fcb0f193e0d2fc0a
Franky333 <1.daniel.frank@gmail.com>
2017-12-22 14:04:45 +0100

    added led hcp handlers

--- src/hcp/hcp_handler_ioState.c
+++ src/hcp/hcp_handler_ioState.c
@@ -2,6 +2,7 @@
 #include "hcp_opcodes.h"
 #include "ringbuffer.h"
 #include "digitalIO.h"
+#include "output.h"
 
 
 void hcp_handler_ioState(hcp_conn_t conn, uint8_t opcode, size_t payloadLength)
@@ -26,7 +27,20 @@ void hcp_handler_ioState(hcp_conn_t conn, uint8_t opcode, size_t payloadLength)
 		return;
 	}
 
-	if(output) //TODO: add leds
+	if(port == HCP_DIGITAL_LED1_PORT)
+	{
+		led1(state);
+		ringbuffer_push(conn.txBuffer, HCP_OK);
+		return;
+	}
+	if(port == HCP_DIGITAL_LED2_PORT)
+	{
+		led2(state);
+		ringbuffer_push(conn.txBuffer, HCP_OK);
+		return;
+	}
+
+	if(output)
 	{
 		digitalIO_setMode(port, PIN_MODE_OUT);
 		digitalIO_setState(port, state);
